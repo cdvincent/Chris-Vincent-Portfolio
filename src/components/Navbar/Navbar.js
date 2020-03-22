796a5b8727a8f65212c4e4ed94f994c6aab1f4e0
Chris Vincent <cdvincent89@gmail.com>
2020-03-21 22:32:31 -0400

    added navbar change position on scroll

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,28 +1,28 @@
 import React from "react";
 import "./style.css";
 
-function Navbar () {
+function Navbar (props) {
     return (
         <div className="wrapper">
-<nav className="navbar navbar-expand-lg flex-md-row navbar-dark bg-dark sticky">
-  <a className="navbar-brand" href="/">Chris Vincent</a>
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse" id="navbarNav">
-    <ul className="navbar-nav">
-      <li className="nav-item active">
-        <a className="nav-link" href="About">About <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="Projects">Projects</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="Contact">Contact</a>
-      </li>
-    </ul>
-  </div>
-</nav>
+            <nav className={props.className}>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                <li className="nav-item active">
+                    <a className="nav-link" href="About">About <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="Projects">Projects</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="Contact">Contact</a>
+                </li>
+                </ul>
+            </div>
+            </nav>
+            <div className="HLine"></div>
         </div>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Jumbotron from "../components/Jumbotron/Jumbotron.js";
 
-function Home(){
 
-    return (
-        <div>
-            <Jumbotron />
-            <Navbar />
-            <div className="container">
-                <h3>Home</h3>
+class Home extends Component {
+
+    state = {
+        activeClass: "navbar-expand-lg flex-md-row navbar-dark bg-dark nav"
+    }
+    
+    componentDidMount() {
+        window.addEventListener("scroll", () => {
+           let activeClass = "navbar-expand-lg flex-md-row navbar-dark bg-dark nav";
+           if(window.scrollY >755){
+               activeClass = "navbar-expand-lg flex-md-row navbar-dark bg-dark sticky";
+           }
+           this.setState({ activeClass: activeClass });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Jumbotron />
+                <Navbar className={this.state.activeClass} />
+                <div className="container">
+                    <h3>Home</h3>
+                </div>
             </div>
-        </div>
-    )
+        )
+    };
 };
 
 export default Home;
