03b99594d2f5135b39e99e98b83dec6d584d79f9
Chris Vincent <cdvincent89@gmail.com>
2020-03-27 19:28:18 -0400

    added fade effect to images on hover

--- src/components/AllProjects/AllProjects.js
+++ src/components/AllProjects/AllProjects.js
@@ -3,19 +3,45 @@ import React from "react";
 function AllProjects () {
     return (
         <div className="images">
-            <img className="projectImg" src={require("./abc.png")} alt="Guess the letter"/>
-            <img className="projectImg" src={require("./burger.jpg")} alt="Burger App"/>
-            <img className="projectImg" src={require("./catGif.gif")} alt="Gif Me Some Gifs!"/>
-            <img className="projectImg" src={require("./friends.jpg")} alt="Friend Finder"/>
-            <img className="projectImg" src={require("./greenRupee.png")} alt="Rupee Game"/>
-            <img className="projectImg" src={require("./hungarray2.png")} alt="Hungarray"/>
-            <img className="projectImg" src={require("./liri.png")} alt="Ask LIRI"/>
-            <img className="projectImg" src={require("./manager.png")} alt="Bamazon"/>
-            <img className="projectImg" src={require("./ow.jpg")} alt="Match The Heroes"/>
-            <img className="projectImg" src={require("./scraping.jpg")} alt="Web Scraper"/>
-            <img className="projectImg" src={require("./stout-beer.jpg")} alt="BrewFinder"/>
-            <img className="projectImg" src={require("./traintime.jpg")} alt="Train Time"/>
-            <img className="projectImg" src={require("./trivia.jpg")} alt="Pokemon Trivia"/>
+            <div>
+                <img className="projectImg" src={require("./abc.png")} alt="Guess the letter"/>
+            </div>
+            <div>
+                <img className="projectImg" src={require("./burger.jpg")} alt="Burger App"/>
+            </div>
+            <div>
+                <img className="projectImg" src={require("./catGif.gif")} alt="Gif Me Some Gifs!"/>
+            </div>
+            <div>
+                <img className="projectImg" src={require("./friends.jpg")} alt="Friend Finder"/>
+            </div>
+            <div>
+                <img className="projectImg" src={require("./greenRupee.png")} alt="Rupee Game"/>
+            </div>
+            <div>
+                <img className="projectImg" src={require("./hungarray2.png")} alt="Hungarray"/>
+            </div>
+            <div>
+                <img className="projectImg" src={require("./liri.png")} alt="Ask LIRI"/>
+            </div>
+            <div>
+                <img className="projectImg" src={require("./manager.png")} alt="Bamazon"/>
+            </div>
+            <div>
+                <img className="projectImg" src={require("./ow.jpg")} alt="Match The Heroes"/>
+            </div>
+            <div>
+                <img className="projectImg" src={require("./scraping.jpg")} alt="Web Scraper"/>
+            </div>
+            <div>
+                <img className="projectImg" src={require("./stout-beer.jpg")} alt="BrewFinder"/>
+            </div>
+            <div>
+                <img className="projectImg" src={require("./traintime.jpg")} alt="Train Time"/>
+            </div>
+            <div>
+                <img className="projectImg" src={require("./trivia.jpg")} alt="Pokemon Trivia"/>
+            </div>
         </div>
     );
 }
