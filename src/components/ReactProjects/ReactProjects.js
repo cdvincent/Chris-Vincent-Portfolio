53a90d2e98c8cc2b508dc19d001b5a6182b52c25
Chris Vincent <cdvincent89@gmail.com>
2020-04-08 14:25:51 -0400

    added animation to projects div on click

--- src/components/ReactProjects/ReactProjects.js
+++ src/components/ReactProjects/ReactProjects.js
@@ -1,12 +1,27 @@
 import React from "react";
 import ProjectImg from "../ProjectImg/ProjectImg"
+import { AnimatePresence, motion } from "framer-motion";
 
+const pageVariants = {
+    initial: {
+      opacity: 0,
+    },
+    in: {
+      opacity: 1,
+    },
+    out: {
+      opacity: 0,
+    }
+  };
+  
 function ReactProjects () {
     return (
-        <div className="images">
-            <ProjectImg src={require("./logo.jpg")} alt="Destination:Budget" text="Destination:Budget" url="https://github.com/cdvincent/Destination-Budget" />
-            <ProjectImg src={require("./ow.jpg")} alt="Memory Game" text="Overwatch Assemble!" url="https://github.com/cdvincent/Overwatch-Assemble" />
-        </div>
+        <AnimatePresence>
+            <motion.div className="images" initial="initial" animate="in" exit="out" variants={pageVariants}>
+                <ProjectImg src={require("./logo.jpg")} alt="Destination:Budget" text="Destination:Budget" url="https://github.com/cdvincent/Destination-Budget" />
+                <ProjectImg src={require("./ow.jpg")} alt="Memory Game" text="Overwatch Assemble!" url="https://github.com/cdvincent/Overwatch-Assemble" />
+            </motion.div>
+        </AnimatePresence>
     );
 }
 
